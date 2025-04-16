b4dc081d28a8d2f6ca57d145b9021e840297ed6c
Lucas <lucas_anjos14@hotmail.com>
2025-04-16 14:53:51 -0300

    Adding deveInteragirComJanelas

--- src/main/java/TesteFramesEJanelas.java
+++ src/main/java/TesteFramesEJanelas.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class TesteFrames {
+public class TesteFramesEJanelas {
     @Test
     public void testeFrames(){
         System.setProperty("webdriver.gecko.driver", "C:\\Drivers\\geckodriver.exe");
@@ -22,5 +22,18 @@ public class TesteFrames {
         driver.switchTo().defaultContent();
         driver.findElement(By.id("elementosForm:nome")).sendKeys(mensagem);
         driver.close();
+    }
+
+    @Test
+    public void deveInteragirComJanelas(){
+        System.setProperty("webdriver.gecko.driver", "C:\\Drivers\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("buttonPopUpEasy")).click();
+        driver.switchTo().window("Popup");
+        driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+        driver.switchTo().window("");
+        driver.findElement(By.tagName("textarea")).sendKeys("e agora?");
     }
 }
