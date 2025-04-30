d237776ab3e9a5e8b3e127e2387a3b39eefe3345
Lucas <lucas_anjos14@hotmail.com>
2025-04-30 16:07:36 -0300

    Refatoração do ExercicioCadastroTest, removendo os métodos de teste, visto que a responsabilidade foi para o arquivo RegrasCadastroTest. Adição de uma suíte de testes.

--- src/main/java/ExercicioCadastroTest.java
+++ src/main/java/ExercicioCadastroTest.java
@@ -8,13 +8,12 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class ExercicioCadastroTest {
     private WebDriver driver;
     private CampoTreinamentoPage page;
-    private DSL dsl;
+
 
     @Before
     public void inicializa(){
         driver = new FirefoxDriver();
         driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
-        dsl = new DSL(driver);
         page = new CampoTreinamentoPage(driver);
     }
 
@@ -41,49 +40,5 @@ public class ExercicioCadastroTest {
         Assert.assertEquals("Escolaridade: superior", page.obterEscolaridade());
         Assert.assertEquals("Esportes: Corrida", page.obterEsporte());
     }
-
-    @Test
-    public void deveValidarNomeObrigatorio(){
-        page.cadastrar();
-        Assert.assertEquals("Nome eh obrigatorio", dsl.alertaObterTextoAlertaEAceita());
-    }
-
-    @Test
-    public void deveValidarSobrenomeObrigatorio(){
-        page.setNome("Lucas");
-        page.cadastrar();
-        Assert.assertEquals("Sobrenome eh obrigatorio", dsl.alertaObterTextoAlertaEAceita());
-    }
-
-    @Test
-    public void deveValidarSexoObrigatorio(){
-        page.setNome("Lucas");
-        page.setSobrenome("Anjos");
-        page.cadastrar();
-        Assert.assertEquals("Sexo eh obrigatorio", dsl.alertaObterTextoAlertaEAceita());
-    }
-
-    @Test
-    public void deveValidarComidaVegetariana(){
-        page.setNome("Lucas");
-        page.setSobrenome("Anjos");
-        page.setSexoMasculino();
-        page.setComidaCarne();
-        page.setComidaVegetariano();
-        page.cadastrar();
-        Assert.assertEquals("Tem certeza que voce eh vegetariano?", dsl.alertaObterTextoAlertaEAceita());
-    }
-
-    @Test
-    public void deveValidarEsportistaIndeciso(){
-        page.setNome("Lucas");
-        page.setSobrenome("Anjos");
-        page.setSexoMasculino();
-        page.setEsporte("Corrida");
-        page.setEsporte("O que eh esporte?");
-        page.cadastrar();
-        Assert.assertEquals("Voce faz esporte ou nao?", dsl.alertaObterTextoAlertaEAceita());
-    }
-
 
 }
